7012d78418edd00825bd5eee2abba12ae2ed6737
winny <winston@ml1.net>
2013-08-10 04:36:19 -0500

    moved to the official googleapis npm package & added support to look up via url

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 var express = require('express');
 var jade = require('jade');
-var restify = require('restify');
-var assert = require('assert');
+var googleapis = require('googleapis');
 
 var settings = require(__dirname + '/settings.json');
 
 var app = express();
 var port = process.env.PORT || 3000;
 var bloggerApiKey = settings.bloggerApiKey;
+var bloggerApiDomain = 'https://www.googleapis.com/';
 app.engine('jade', jade.__express);
 app.set('view engine', 'jade');
+
+
+function getBlogId(url, callback){
+  url = 'http://'+url+'/';
+  googleapis.discover('blogger', 'v3').execute(function (err, client){
+    var parameters = {
+      url: url,
+      key: bloggerApiKey
+    }
+    var req1 = client.blogger.blogs.getByUrl(parameters);
+    req1.execute(function (err, response){
+      var id = false;
+      if (!err) {
+        id = response.id;
+      }
+      callback(id);
+    });
+  });
+}
+
+function getBlog(id, callback){
+  googleapis.discover('blogger', 'v3').execute(function (err, client){
+    var parameters1 = {
+      blogId: id,
+      key: bloggerApiKey
+    }
+    req1 = client.blogger.blogs.get(parameters1);
+    req1.execute(function (err, response1){
+      var parameters2 = {
+        blogId: id,
+        maxResults: 1,
+        key: bloggerApiKey
+      }
+      req2 = client.blogger.posts.list(parameters2);
+      req2.execute(function (err, response2){
+        callback(response1, response2);
+      });
+    });
+  });
+}
 
 app.get('/', function (req, res){
   res.set('Content-Type', 'text/plain');
@@ -18,21 +58,22 @@ app.get('/', function (req, res){
 });
 
 app.get('/blog/:id', function (req, res){
-  console.log('Request for blog resource "'+req.params.id+'".');
   res.set('Content-Type', 'text/html; charset=utf-8');
 
-  var client = restify.createJsonClient({
-    url: 'https://www.googleapis.com/blogger/v3/blogs/'
-  });
-
-  client.get('/blogger/v3/blogs/'+req.params.id+'?key='+bloggerApiKey, function (err, req_, res_, obj){
-    assert.ifError(err);
-    client.get('/blogger/v3/blogs/'+req.params.id+'/posts?key='+bloggerApiKey, function (err, req__, res__, obj_){
-      assert.ifError(err);
-      res.render('blog.jade', {blog:obj, posts:obj_});
+  var id = req.params.id;
+  if (!isNaN(id)){
+    getBlog(id, function (blog, posts){
+      res.render('blog.jade', {blog:blog, posts:posts});
     });
-  });
+  } else {
+    getBlogId(id, function(id){
+      getBlog(id, function (blog, posts){
+        res.render('blog.jade', {blog:blog, posts:posts});
+      });
+    });
+  }
 });
+
 
 console.log('Listening on port: '+port+'.');
 app.listen(port);
